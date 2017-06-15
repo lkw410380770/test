edcb6929cd240e2f89f159d84a5a1aa57a35e40d
lkw410380770 <410380770@qq.com>
2017-06-15 16:11:55 +0800

    json练习

---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('hello,node');
+/**
+ * [getData description]
+ * @param  {[type]} data [description]
+ * @return {[type]}      [description]
+ */
+function getData(data){
+	var sta = [];
+	return sta;
+
+}
